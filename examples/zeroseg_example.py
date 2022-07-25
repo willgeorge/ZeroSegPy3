52d61354bf71052a0949264e05001fd3f0bf8b1c
Will George <zebbodee@gmail.com>
2022-07-25 12:40:35 +0100

    Update zeroseg_example.py
    Getting rid of xrange

--- examples/zeroseg_example.py
+++ examples/zeroseg_example.py
@@ -27,7 +27,7 @@ def date(device, deviceId):
 
 def clock(device, deviceId, seconds):
 
-    for _ in xrange(seconds):
+    for _ in range(seconds):
         now = datetime.now()
         hour = now.hour
         minute = now.minute
